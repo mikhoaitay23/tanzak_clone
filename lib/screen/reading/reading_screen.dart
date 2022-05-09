a2a89a0e6a0a68316517f69afb4046e5060f656c
unknown <thanhnv@eqtek.vn>
2022-05-09 18:01:34 +0700

    - update

--- lib/screen/reading/reading_screen.dart
+++ lib/screen/reading/reading_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_bubble/bubble_type.dart';
+import 'package:flutter_chat_bubble/chat_bubble.dart';
+import 'package:flutter_chat_bubble/clippers/chat_bubble_clipper_8.dart';
 
+import '../../data/model/message_model.dart';
 import '../../widget/ktext.dart';
 
 class ReadingScreen extends StatefulWidget {
@@ -9,37 +13,85 @@ class ReadingScreen extends StatefulWidget {
 
   @override
   ReadingScreenState createState() => ReadingScreenState();
-
 }
 
 class ReadingScreenState extends State<ReadingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Column(
-          children: [
-            KText(text: 'Name', color: Colors.white,),
-            KText(text: 'Page', color: Colors.white)
+        appBar: AppBar(
+          title: Column(
+            children: [
+              KText(
+                text: 'Name',
+                color: Colors.white,
+              ),
+              KText(text: 'Page', color: Colors.white)
+            ],
+          ),
+          centerTitle: true,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.more_horiz),
+              tooltip: 'Option',
+            )
           ],
         ),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {
+        body: Column(
+          children: [
+            Expanded(
+                child: ClipRRect(
+              child: ListView.builder(
+                reverse: true,
+                padding: const EdgeInsets.only(top: 15.0),
+                itemCount: messages.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final Message message = messages[index];
+                  final bool isMe = message.sender.id == currentUser.id;
+                  return _buildMessage(message, isMe);
+                },
+              ),
+            ))
+          ],
+        ));
+  }
 
-            },
-            icon: const Icon(Icons.open_with),
-            tooltip: 'Option',
-          )
-        ],
-      ),
-      body: Center (
-        child: KText(
-          text: 'asdas',
+  _buildMessage(Message message, bool isMe) {
+    if (isMe) {
+      return ChatBubble(
+        clipper: ChatBubbleClipper8(type: BubbleType.sendBubble),
+        alignment: Alignment.topRight,
+        margin: const EdgeInsets.only(top: 20),
+        backGroundColor: Colors.blue,
+        child: Container(
+          constraints: BoxConstraints(
+            maxWidth: MediaQuery.of(context).size.width * 0.7,
+          ),
+          child: Text(
+            message.text,
+            style: const TextStyle(color: Colors.white),
+          ),
+        ),
+      );
+    }
+    return ChatBubble(
+      clipper: ChatBubbleClipper8(type: BubbleType.receiverBubble),
+      backGroundColor: const Color(0xffE7E7ED),
+      margin: const EdgeInsets.only(top: 20),
+      child: Container(
+        constraints: BoxConstraints(
+          maxWidth: MediaQuery.of(context).size.width * 0.7,
+        ),
+        child: Text(
+          message.text,
+          style: const TextStyle(color: Colors.black),
         ),
       ),
     );
   }
-
 }
